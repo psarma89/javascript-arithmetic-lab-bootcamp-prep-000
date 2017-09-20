81f68397faa32d639b44d3bdb9941a2b0590f599
Priyam Sarma <priyam.sarma@gmail.com>
2017-09-20 14:39:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-function add(a,b){}
+function add(a,b){
+  return a+b
+}
 
-function subtract(a,b){}
+function subtract(a,b){
+  return a-b
+}
 
-function multiply(a,b){}
+function multiply(a,b){
+  return a*b
+}
 
-function divide(a,b){}
+function divide(a,b){
+  return a/b
+}
 
 function inc(n){}
 
 function dec(n){}
+
+function makeInt(n){}
