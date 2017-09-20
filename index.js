c2774831987ddaddb20c422f7bfc2c7ab2da2773
Priyam Sarma <priyam.sarma@gmail.com>
2017-09-20 14:37:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-function add(a,b){
-  
-}
+function add(a,b){}
 
-function subtract(a,b){
-  
-}
+function subtract(a,b){}
 
-function multiply(a,b){
-  
-}
+function multiply(a,b){}
 
-function divide(a,b){
-  
-}
+function divide(a,b){}
 
+function inc(n){}
+
+function
